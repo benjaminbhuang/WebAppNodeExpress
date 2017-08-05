c685966c6058176952971a139c5e5299d6a18744
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-08-05 01:49:52 -0400

    add auth user & pass

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
 var app = express();
 var port = process.env.PORT || 5000;
 
@@ -29,8 +34,21 @@ var nav = [
 var authorRouter = express.Router();
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter= require('./src/routes/adminRoutes')(nav);
+var authRouter= require('./src/routes/authRoutes')(nav);
 
+//middleware section
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(session({
+    secret: 'library',
+    resave: true,
+    saveUninitialized: true
+}));
+
+require('./src/config/passport')(app);
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
@@ -52,6 +70,8 @@ app.use('/Books', bookRouter);
 app.use('/Authors', authorRouter);
 
 app.use('/Admin', adminRouter);
+
+app.use('/Auth', authRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {title: 'Index',
---
+++ src/config/strategies/local.strategy.js
@@ -0,0 +1,34 @@
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    mongodb = require('mongodb').MongoClient;
+
+module.exports = function () {
+    passport.use(new LocalStrategy({
+            usernameField: 'userName',
+            passwordField: 'password'
+        },
+        function (username, password, done) {
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('user');
+
+                collection.findOne({
+                        username: username
+                    },
+                    function (err, results) {
+
+                        if (err) {
+                            console.log('ERROR: ' + err.message);
+                        }
+                        else {
+                            if (results.password === password) {
+                                var user = results;
+                                done(null, user);
+                            } else {
+                                done(null, false, {message: 'Bad password'});
+                            }
+                        }
+                    });
+            });
+        }));
+};
